eaecc38f4f4ceacaab6165f8948b658256ed32c2
Mr.V <dzulajvita@gmail.com>
2023-04-22 01:15:05 +0300

    added react to the site

---
+++ installation/my_app01/src/like_button.js
@@ -0,0 +1,15 @@
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) return 'You liked this.';
+
+    return <button onClick={() => this.setState({ liked: true })}>Like</button>;
+  }
+}
+
+let domContainer = document.querySelector('#like_btn_container');
+ReactDOM.render(<LikeButton />, domContainer);
