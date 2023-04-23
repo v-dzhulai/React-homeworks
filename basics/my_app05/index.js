2a44bb1af4d4d88b5de9af3e65c09539327c0db8
Mr.V <dzulajvita@gmail.com>
2023-04-24 00:41:09 +0300

    conditional rendering

---
+++ basics/my_app05/index.js
@@ -0,0 +1,53 @@
+function UserGreeting(props) {
+  return <h1>Welcome back!</h1>;
+}
+
+function GuestGreeting(props) {
+  return <h1>Login please.</h1>;
+}
+
+function Greeting(props) {
+  return props.isLoggedIn ? <UserGreeting /> : <GuestGreeting />;
+}
+
+function LoginButton(props) {
+  return <button onClick={props.onClick}>Login</button>;
+}
+
+function LogoutButton(props) {
+  return <button onClick={props.onClick}>Logout</button>;
+}
+
+class LoginControl extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isLoggedIn: false };
+  }
+
+  handleLoginClick = () => {
+    this.setState({ isLoggedIn: true });
+  };
+
+  handleLogoutClick = () => {
+    this.setState({ isLoggedIn: false });
+  };
+
+  render() {
+    const isLoggedIn = this.state.isLoggedIn;
+    let button = isLoggedIn ? (
+      <LogoutButton onClick={this.handleLogoutClick} />
+    ) : (
+      <LoginButton onClick={this.handleLoginClick} />
+    );
+
+    return (
+      <div>
+        <Greeting isLoggedIn={isLoggedIn} />
+        {button}
+      </div>
+    );
+  }
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<LoginControl />);
